74099ebc03e515b92219da4e179edb11083389f8
Rohithakur027 <157215457+Rohithakur027@users.noreply.github.com>
2024-01-23 13:49:29 +0000

    container

---
+++ 01_basics/date.js
@@ -0,0 +1,59 @@
+// Dates
+
+// let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+
+
+// console.log(myDate.toISOString());
+
+
+// console.log(myDate.toJSON());
+
+// console.log(myDate.toLocaleDateString());
+
+
+// console.log(myDate.toLocaleString());
+
+
+// console.log(myDate.toLocaleTimeString());
+
+// console.log(myDate.toTimeString());
+
+// console.log(typeof (myDate));
+
+// let myCreatedDate = new Date(2004,2,7)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate1 = new Date(2004 , 2 , 7 , 5 , 3)
+// console.log(myCreatedDate.toLocaleString());
+
+
+// let myDate = new Date("2004-03-07")
+// console.log(myDate.toDateString());
+
+
+let myDate = new Date("01-03-2004")
+// console.log(myDate.toDateString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myDate.getTime()); 
+
+// console.log(myDate.getTime()); 
+
+// console.log(Math.round (myDate)/1000); 
+
+
+let newDate = new Date()
+
+// console.log(myDate.toLocaleString('deafult',{weekday:"long"} ));
+console.log(myDate.toLocaleString('deafult',{weekday:"long"} ));
+
+
+
+
+
+
