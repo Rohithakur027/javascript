28e66c5f2803c0c147713550cb9b635731661004
Rohithakur027 <157215457+Rohithakur027@users.noreply.github.com>
2024-01-24 00:11:47 +0000

    container

---
+++ 02_basics/01_array.js
@@ -0,0 +1,53 @@
+// Array
+
+// const myArray= [0, 1, 2, 3, 4, 5]
+
+// const myHeros = ["shakriman","rohit"]
+
+const myArr = new Array(1, 2, 3, 5, 6, 7)
+
+// console.log(myArr2);
+
+// // Array methods 
+
+// myArr.push(6)
+// myArr.push(8)
+// console.log(myArr);
+// myArr.pop()
+
+// console.log(myArr);
+
+// myArr.unshift(7)
+// myArr.shift()
+
+
+
+
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(2));
+// console.log(myArr);
+
+
+// const newArr = myArr.join()  //converts into string
+
+// console.log(newArr);
+
+// Slice ,Splice
+
+console.log("A" , myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+
+console.log("B" , myArr);
+
+const  myn2 = myArr.splice(1,3)
+
+console.log(myn2);
+
+console.log( "C" , myArr)
+
+
+// slice does not include last index while splice does 
+// the original array change in splice methods
